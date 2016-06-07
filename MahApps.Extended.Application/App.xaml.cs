eea232bdb840be7c2487aa1d771bfbb11e48576e
ICodeMyOwnLife <icodemyownlife@gmail.com>
2016-06-07 20:26:02 +0700

    First commit

--- MahApps.Extended.Application/App.xaml.cs
+++ MahApps.Extended.Application/App.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-
-namespace MahApps.Extended.Application
+﻿namespace MahApps.Extended.Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
+    public partial class App { }
 }
